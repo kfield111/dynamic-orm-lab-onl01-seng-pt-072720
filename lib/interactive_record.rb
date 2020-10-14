937af92b408f114f575ac027f17894cd0d001a7f
Kelin Field <kfield111@hotmail.com>
2020-10-14 02:35:29 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,8 +54,8 @@ class InteractiveRecord
   end
 
   def self.find_by(attribute)
-    sql = "SELECT * FROM #{self.table_name} WHERE ? = ?"
-    DB[:conn].execute(sql, attribute)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys[0].to_s} = ?"
+    DB[:conn].execute(sql, (attribute.values[0]))
   end
 
 end
