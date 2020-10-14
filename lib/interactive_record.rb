91bec2541e09d01bb07bc77f5f0500588f94bb6c
Kelin Field <kfield111@hotmail.com>
2020-10-14 02:14:47 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,4 +7,17 @@ class InteractiveRecord
     self.to_s.downcase.pluralize
   end
 
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = "pragma table_data ('#{table_name}')"
+
+    table_data = DB[:conn].execute(sql)
+    column_names = []
+    table_data.each do |temp|
+      column_names << temp["name"]
+    end
+    column_names.compact
+  end
+
 end
